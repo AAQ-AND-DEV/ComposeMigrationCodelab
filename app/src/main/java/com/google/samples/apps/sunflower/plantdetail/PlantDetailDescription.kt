8ce0563b5220fda4d7cb4bd4a222452f28052c53
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-04-28 17:30:58 -0700

    * Converts from xml-based constraintLayout to Composable-based content    - PlantDetailDescription.kt accepts a PlantDetailViewModel    - observeAsState of plant, passes that plant to PlantDetailContent    - PlantDetailContent contains column with the name, interval, desc Texts    - Needed to use AndroidView to hold a TextView, in order to use the fromHtml function to access the html formatted text

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,137 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
-fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
+fun PlantDetailDescription(plantDetailVM: PlantDetailViewModel) {
+    val plant by plantDetailVM.plant.observeAsState()
+    
+    plant?.let{
+     PlantDetailContent(plant = it)
+    }
+}
+
+@Composable
+fun PlantDetailContent(plant: Plant){
+    Surface{
+        Column(Modifier.padding(dimensionResource(R.dimen.margin_normal))){
+            PlantName(plant.name)
+            PlantWatering(wateringInterval = plant.wateringInterval)
+            PlantDescription(desc = plant.description)
+        }
+    }
+}
+
+@Composable
+fun PlantName(name: String){
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Composable
+private fun PlantWatering(wateringInterval: Int){
+    Column(Modifier.fillMaxWidth()){
+        val centerWithPaddingModifier = Modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .align(Alignment.CenterHorizontally)
+
+        val normalPadding = dimensionResource(id = R.dimen.margin_normal)
+
+        Text(
+            text = stringResource(R.string.watering_needs_prefix),
+            color = MaterialTheme.colors.primaryVariant,
+            fontWeight = FontWeight.Bold,
+            modifier = centerWithPaddingModifier.padding(top = normalPadding)
+        )
+
+        val wateringIntervalText = LocalContext.current.resources.getQuantityString(
+            R.plurals.watering_needs_suffix, wateringInterval, wateringInterval
+        )
+        Text(
+            text = wateringIntervalText,
+            modifier = centerWithPaddingModifier.padding(bottom=normalPadding)
+        )
+    }
+}
+
+@Composable
+private fun PlantDescription(desc: String){
+    //rememembers HTML formatted desc, reexecutes on new desc
+    val htmlDesc = remember(desc){
+        HtmlCompat.fromHtml(desc, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+    AndroidView(
+        factory = {context ->
+            TextView(context).apply{
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDesc
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun PlantDetailContentPreview(){
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MaterialTheme() {
+        PlantDetailContent(plant = plant)
+    }
+}
+@Preview
+@Composable
+private fun PlantNamePreview(){
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MaterialTheme() {
+        PlantDetailContent(plant)
+    }
+}
+
+@Preview
+@Composable
+private fun PlantWateringPreview(){
+    MaterialTheme() {
+        PlantWatering(7)
+    }
+}
+
+@Preview
+@Composable
+private fun PlantDescPreview(){
+    MaterialTheme() {
+        PlantDescription(desc = "HTML<br><br>description")
     }
 }
