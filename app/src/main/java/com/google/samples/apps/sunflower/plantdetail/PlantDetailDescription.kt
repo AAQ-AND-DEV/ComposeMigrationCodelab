a5892c84e48e4f8682c11da9710f3e9ea68fe3ab
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-04-29 08:06:25 -0700

    * uses MdcTheme to access XML-based Material theme for compose components * adds darkTheme preview to PlantDetailDescription.kt * rewrites testing of detailFragment to use ComposeRule

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.foundation.layout.Column
@@ -38,6 +39,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -122,7 +124,7 @@ private fun PlantDescription(desc: String){
 @Composable
 private fun PlantDetailContentPreview(){
     val plant = Plant("id", "Apple", "description", 3, 30, "")
-    MaterialTheme() {
+    MdcTheme {
         PlantDetailContent(plant = plant)
     }
 }
@@ -130,15 +132,15 @@ private fun PlantDetailContentPreview(){
 @Composable
 private fun PlantNamePreview(){
     val plant = Plant("id", "Apple", "description", 3, 30, "")
-    MaterialTheme() {
-        PlantDetailContent(plant)
+    MdcTheme {
+        PlantName(plant.name)
     }
 }
 
 @Preview
 @Composable
 private fun PlantWateringPreview(){
-    MaterialTheme() {
+    MdcTheme {
         PlantWatering(7)
     }
 }
@@ -146,7 +148,16 @@ private fun PlantWateringPreview(){
 @Preview
 @Composable
 private fun PlantDescPreview(){
-    MaterialTheme() {
+    MdcTheme {
         PlantDescription(desc = "HTML<br><br>description")
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PlantDetailContentDarkPreview(){
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MdcTheme() {
+        PlantDetailContent(plant = plant)
     }
 }
